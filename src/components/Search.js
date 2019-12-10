c410894ede243c9c9f3da94f8760285045d52f3d
Jelle Verzijden <jelle@devmen.nl>
2019-12-10 20:23:36 +0100

    fix: change query comparison to regEx to fix case sensitivity issue

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,22 +4,24 @@ import config from '../config';
 export const Search = async (event, parent) => {
   const parentNode = document.getElementById(parent);
   const searchResultField = document.createElement('ul');
-  searchResultField.id = config.SEARCH_LIST_ID;
-  searchResultField.classList = 'search-list card';
   const container = document.getElementById(config.SEARCH_LIST_ID);
+  searchResultField.id = config.SEARCH_LIST_ID;
+  searchResultField.classList = 'search-list card shadow';
   const { value } = event.target;
-
-  if (value && value.length > 1 && !container) {
-    parentNode.appendChild(searchResultField);
-  }
 
   const mapResults = (result) => result.map((item, idx) => (
     `<li id="q-listitem-${idx}" class="rw-autosuggest__results-flights">${item.airport}</li>`
   )).join('');
 
+  if (value && value.length > 1 && !container) {
+    parentNode.appendChild(searchResultField);
+  }
+
   if (value && value.length > 2 && container) {
+    const regExp = new RegExp(value, 'i');
+
     const data = await request(config.API_URL, []);
-    const result = data.flights.filter((item) => item.airport.includes(value));
+    const result = data.flights.filter((item) => item.airport.match(regExp));
     container.innerHTML = mapResults(result);
   }
   if (value && value.length < 3 && container) {
