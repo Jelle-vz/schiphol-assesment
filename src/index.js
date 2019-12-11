06c4070b93a7646d7758edd7f515b648cced45a9
Jelle Verzijden <jelle@devmen.nl>
2019-12-11 15:45:51 +0100

    refactor: refactor index to work with initial data

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ import config from './config';
 import { SearchResult } from './components/SearchResult';
 import { FlightInfo } from './components/FlightInfo';
 
+// Utils
+import { request } from './utils/requestHelper';
+
 // Assets
 import bgImage from './assets/svg/woman-sitting';
 import '../sass/index.scss';
 
-document.getElementById('hero_image').innerHTML = bgImage;
-document.getElementById('search')
-  .addEventListener('keyup', (event) => SearchResult(event, config.SEARCH_RESULT_WRAPPER_ID));
-document.addEventListener('click', (event) => {
-  if (event.target && event.target.id.startsWith('q-listitem-')) {
-    FlightInfo(event, config.SEARCH_RESULT_WRAPPER_ID);
-  }
-});
+const initialDataLoaded = (data) => {
+  const { flights } = data;
+  document.getElementById('main').classList = '';
+  document.getElementById('hero_image').innerHTML = bgImage;
+
+  document.getElementById('search')
+    .addEventListener('keyup', (event) => SearchResult(event, config.SEARCH_RESULT_WRAPPER_ID, flights));
+
+  document.addEventListener('click', (event) => {
+    if (event.target && event.target.id.startsWith('q-listitem-')) {
+      FlightInfo(event, config.SEARCH_RESULT_WRAPPER_ID, flights);
+    }
+  });
+};
+
+request(config.API_URL, []).then((response) => initialDataLoaded(response));
