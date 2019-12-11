e2eb40cc0faa81156dbf98f3b6fdf30ffd3857cc
Jelle Verzijden <jelle@devmen.nl>
2019-12-11 11:38:17 +0100

    refactor: move flights data to static folder
    To be able to use the flight data  in a production build we need to move it in the build process. Renamed the `public` folder to `static` to prevent issues where some server automatically run the contents of a folder called public.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_URL: '/src/data/flights.json',
+  API_URL: '/static/flights.json',
   SEARCH_RESULT_WRAPPER_ID: 'search_result_container',
   SEARCH_LIST_ID: 'search-result-list',
   RESULT_CONTAINER_ID: 'search_result',
